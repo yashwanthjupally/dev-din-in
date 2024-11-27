bf1b287c0dae518fb5af3a114aa25f24738ffa6c
PC <jupallyyashwanth376@gmail.com>
2024-11-27 19:39:48 +0530

    Responsive Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,28 @@
+import { GitHub, Instagram, Twitter, YouTube } from '@mui/icons-material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Box sx={{ textAlign: 'center', bgcolor: '#1A1A19', color: 'white', padding: 1.5 }}>
+
+        <Box sx={{
+          my: '2vh',
+          '& svg': { fontSize: '2rem', mr: 2, cursor: 'pointer' },
+          '& svg:hover': { color: 'goldenrod', transform: 'translatex(5px)', transition: 'all 400ms' }
+        }}>
+          <Instagram />
+          <Twitter />
+          <GitHub />
+          <YouTube />
+        </Box>
+
+        <Typography variant='h5' sx={{ '@media (max-width: 600px)': { fontSize: '1rem' } }}>
+          All Rights Reserved &copy; Yashwanth
+        </Typography>
+      </Box>
+    </>
   )
 }
 
